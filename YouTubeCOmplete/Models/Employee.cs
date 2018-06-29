71c6da249d49dd68e9f05ef08ca2a3b4f4a8bd16
Akhilesh Kumar Sahu <akhileshsh026@hotmail.com>
2018-06-29 18:40:52 +0530

    Map the model class Employee with the Datbase table named TblEmployee with the help of Data Annotations .

--- YouTubeCOmplete/Models/Employee.cs
+++ YouTubeCOmplete/Models/Employee.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace YouTubeCOmplete.Models
 {
+    [Table("TblEmployee")]
     public class Employee
     {
         public int Id { get; set; }
